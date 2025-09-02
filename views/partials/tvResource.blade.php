3c983878eb7b2aec7a13a0e18a3951341bc4beb7
Serhii Korneliuk <info@seigerit.com>
2025-09-02 13:00:01 +0300

    [FIX] Hide left column if TV is Multifields.

--- views/partials/tvResource.blade.php
+++ views/partials/tvResource.blade.php
@@ -1,14 +1,16 @@
 <div class="row form-row">
     <div class="row-col col-lg-12 col-12">
         <div class="row form-row">
-            <div class="col-auto col-title-auto">
-                <label for="{{$row['name']}}" class="warning">
-                    {{$row['caption']}}
-                    @if(evo()->hasPermission('edit_template'))<br/><small class="text-muted">[*{{$row['name']}}*]</small>@endif
-                    @if(substr(($tvPBV??''), 0, 8) == '@INHERIT')<br/><small class="comment inherited">({{$_lang['tmplvars_inherited']}})</small>@endif
-                </label>
-                @if(!empty($row['description']))<i class="{{$_style["icon_question_circle"]}}" data-tooltip="{{$row['description']}}"></i>@endif
-            </div>
+            @if(!in_array($row['type'], ['custom_tv:multifields']))
+                <div class="col-auto col-title-auto">
+                    <label for="{{$row['name']}}">
+                        {{$row['caption']}}
+                        @if(evo()->hasPermission('edit_template'))<br/><small class="text-muted">[*{{$row['name']}}*]</small>@endif
+                        @if(substr(($tvPBV??''), 0, 8) == '@INHERIT')<br/><small class="comment inherited">({{$_lang['tmplvars_inherited']}})</small>@endif
+                    </label>
+                    @if(!empty($row['description']))<i class="{{$_style["icon_question_circle"]}}" data-tooltip="{{$row['description']}}"></i>@endif
+                </div>
+            @endif
             <div class="col">
                 {!! renderFormElement(
                     $row['type'],
